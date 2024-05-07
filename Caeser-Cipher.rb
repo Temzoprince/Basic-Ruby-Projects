1d1454654eee6e7b24dac08045d70621da2431db
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-05-07 09:55:29 +0100

    Use ascii code
    Ascii code was used instead of alphabet array to shift values
    Allowed me to isolate the characters for shifting only

--- Caeser-Cipher.rb
+++ Caeser-Cipher.rb
@@ -1,37 +1,29 @@
 # require 'pry-byebug'
 
-alphabet = ('a'..'z').to_a
-# puts alphabet
-
 def caesar_cipher(string, key)
-    alphabet = ('a'..'z').to_a
-    # puts alphabet
-
-    # binding.pry
-
+    # # binding.pry
 
     array_string = string.split("")
     p array_string
     encrypt_array = array_string.map do |letter|
-        if letter == ' ' || letter == '!'
-            next
+        encrypt_letter = letter.ord
+        if encrypt_letter >= 65 && encrypt_letter <= 90
+            encrypt_letter += 5
+            if encrypt_letter > 90
+                encrypt_letter -= 26
+            end
         end
-        upcase = true if letter.upcase == letter
-        shift_value = alphabet.index(letter.downcase) + key
-        p shift_value
-        if shift_value > 25
-            shift_value = shift_value - 26
+        if encrypt_letter >= 97 && encrypt_letter <= 122
+            encrypt_letter += 5
+            if encrypt_letter > 122
+                encrypt_letter -= 26
+            end
         end
-        if letter == ' ' || letter == '!'
-            letter
-        elsif upcase
-            letter = alphabet[shift_value].upcase
-        else
-            letter = alphabet[shift_value]
-        end
+        letter = encrypt_letter.chr
     end
     p encrypt_array
     encrypt_array.join("")
+
 end
 
 puts caesar_cipher("What a string!", 5)
