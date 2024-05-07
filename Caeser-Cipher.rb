20239b402732ba09aac72bc114d5b93e3cd754f9
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-05-07 06:23:43 +0100

    Shifts values key number of spaces
    Error is created because special characters do not space in array

---
+++ Caeser-Cipher.rb
@@ -0,0 +1,36 @@
+# require 'pry-byebug'
+
+alphabet = ('a'..'z').to_a
+# puts alphabet
+
+def caesar_cipher(string, key)
+    alphabet = ('a'..'z').to_a
+    # puts alphabet
+
+    # binding.pry
+
+
+    array_string = string.split("")
+    p array_string
+    encrypt_array = array_string.map do |letter|
+        upcase = true if letter.upcase == letter
+        shift_value = alphabet.index(letter.downcase) + key
+        p shift_value
+        if shift_value > 25
+            shift_value = shift_value - 26
+        end
+        if letter == ' ' || letter == '!'
+            letter
+        elsif upcase
+            letter = alphabet[shift_value].upcase
+        else
+            letter = alphabet[shift_value]
+        end
+    end
+    p encrypt_array
+    encrypt_array.join("")
+end
+
+puts caesar_cipher("What a string!", 5)
+
+puts alphabet.find_index('z')
