cee8c89e7b5d29dc66a9fe24bc93c26c9df8c455
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-06-05 21:14:34 +0100

    Substrings works with multiple words

--- Sub-Strings.rb
+++ Sub-Strings.rb
@@ -2,25 +2,32 @@
 
 def substrings(string, dictionary)
   i = 0
+  words = string.split(' ')
   substrings = Hash.new(0)
   until i == dictionary.length
-    p dictionary[i]
-    substrings[dictionary[i]] += 1 if string.include? dictionary[i]
+    words.each do |word|
+      # p dictionary[i]
+      substrings[dictionary[i]] += 1 if word.downcase.include? dictionary[i]
+    end
     i += 1
   end
   substrings
 end
 
 def substrings2(string, dictionary)
+  words = string.split(' ')
   dictionary.each_with_object(Hash.new(0)) do |substring, list|
-    list[substring] += 1 if string.include? substring
-    # p list
+    words.each do |word|
+      list[substring] += 1 if word.downcase.include? substring
+      # p list
+    end
   end
 end
 
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
 # puts substrings("below", dictionary)
-puts substrings2('below', dictionary)
+# puts substrings2('below', dictionary)
 
-# substrings2("Howdy partner, sit down! How's it going?", dictionary)
+# puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+puts substrings2("Howdy partner, sit down! How's it going?", dictionary)
