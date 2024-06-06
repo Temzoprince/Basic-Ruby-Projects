edf461879f5435c9569d02578abf8a0ed8316782
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-06-06 18:48:05 +0100

    Added files to folders

---
+++ stock_picker/stock_picker.rb
@@ -0,0 +1,21 @@
+input = [17, 3, 6, 9, 15, 8, 6, 1, 10]
+
+def stock_picker(stocks)
+  profit = 0
+  best_buy = 0
+  best_sell = 0
+  stocks.each_with_index do |value, index|
+    i = index + 1
+    until i == stocks.length
+      if (stocks[i] - value) > profit
+        profit = (stocks[i] - value)
+        best_buy = value
+        best_sell = stocks[i]
+      end
+      i += 1
+    end
+  end
+  [stocks.index(best_buy), stocks.index(best_sell)]
+end
+
+p stock_picker(input)
