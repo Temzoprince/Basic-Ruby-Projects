3a2c2daba5fde8a1ba7991d9900b225663b2ae25
Temzoprince <108231970+Temzoprince@users.noreply.github.com>
2024-06-06 18:55:43 +0100

    Rubocop

--- substrings/sub_strings.rb
+++ substrings/sub_strings.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-# Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument. 
+# Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument.
 # It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found.
-
 
 def substrings(string, dictionary)
   words = string.split(' ')
@@ -15,6 +14,6 @@ end
 
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
-puts substrings("below", dictionary)
+puts substrings('below', dictionary)
 
 puts substrings("Howdy partner, sit down! How's it going?", dictionary)
